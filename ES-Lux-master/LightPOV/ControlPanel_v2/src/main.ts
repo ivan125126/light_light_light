7a0a2de23aa2454e46f29283a5fda9c5057debcf
CandleHsu <57028390+KendellHsu@users.noreply.github.com>
2026-04-04 06:33:42 +0800

    feat: implement PreviewPanel, fix auto-update bug

--- ES-Lux-master/LightPOV/ControlPanel_v2/src/main.ts
+++ ES-Lux-master/LightPOV/ControlPanel_v2/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import './css/style.css'
+// pre_view Web Component（Plan 3 遷移為 PreviewElement.ts）
+import './js/pre_view.js'
 
 const app = createApp(App)
 app.use(createPinia())
